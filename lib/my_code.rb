29e4616c7756eabbcbd327651c5ed58bd929b8f6
Aniket B <aniket.b@outlook.com>
2019-09-02 14:46:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,3 +16,14 @@ def map_to_square(source_array)
   source_array.map { |i| i * i }
 end
 
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end

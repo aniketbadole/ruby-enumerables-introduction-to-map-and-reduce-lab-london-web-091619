06d9455a2e1c049766b27e1ae80b733820aba37e
Aniket B <aniket.b@outlook.com>
2019-09-02 14:40:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,9 @@
 # My Code here....
 
-def map_to_negativize(array)
-  array.map { |i| i * -1 }
+def map_to_negativize(source_array)
+  source_array.map { |i| i * -1 }
 end
 
-def map_
+def map_to_no_change(source_array)
+  source_array.map { |i| i }
+end

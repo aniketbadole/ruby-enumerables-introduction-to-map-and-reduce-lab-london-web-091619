5b3b6fadbd6cb316c1a246f58de6e76c03dee29f
Aniket B <aniket.b@outlook.com>
2019-09-02 14:41:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,10 @@ def map_to_no_change(source_array)
   source_array.map { |i| i }
 end
 
+def map_to_double(source_array)
+  source_array.map { |i| i * 2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |i| i * i }
+end

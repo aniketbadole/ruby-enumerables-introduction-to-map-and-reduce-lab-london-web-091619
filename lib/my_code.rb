9b04afeb8d7aa66b514afa47c3e05a10f38057d7
Aniket B <aniket.b@outlook.com>
2019-09-02 14:34:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,15 +1,3 @@
 def map_to_negativize(source_array) {
   return source_array.map { |i| i * -1 }
 }
-
-def map_to_no_change(source_array) {
-  
-}
-
-def map_to_double(source_array) {
-  
-}
-
-def map_to_square(source_array) {
-  
-}

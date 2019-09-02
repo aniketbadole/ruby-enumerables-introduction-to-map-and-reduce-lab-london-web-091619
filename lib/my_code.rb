27957652f031bca29b9250e93db555bc244724de
Aniket B <aniket.b@outlook.com>
2019-09-02 14:57:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,7 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-  
+  source_array.reduce(0) { |sum, n| sum + n }
 end
 
 def reduce_to_any_true(source_array)
